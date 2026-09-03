93e841f8ca8228ddf12b3c22bab4001bb3b15b83
Ahmad Bahrami <medeedabteilung95@gmail.com>
2026-09-03 23:35:40 +0330

    Add service worker

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+const CACHE='milich-pm-v2';
+const BASE=self.location.pathname.replace(/\/sw\.js$/,'');
+const CORE=[`${BASE}/`,`${BASE}/icon.svg`];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request).then(r=>r||caches.match(`${BASE}/`))));
+});
